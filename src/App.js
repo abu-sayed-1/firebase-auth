2800c1dfa39351633c18b523e1ca80f2b76566c3
as ahmed <rakibasrr13@gmail.com>
2020-09-10 21:58:29 +0300

    Registration form

--- src/App.js
+++ src/App.js
@@ -47,26 +47,101 @@ const provider = new firebase.auth.GoogleAuthProvider();
      }
      setUser(signedOutUser)
    })
-   .catch( error =>{
+   .catch( error => {
 
    })
  }
-  return (
-    <div>
-      {
-        user.isSignIn ?<button onClick={handleSignOut}>sign out</button>:
 
-                      <button onClick={handleSingIn}>sign in</button>
+const handleSubmit = () => {
+
+}
+
+const handleBlur = (event) => {
+  console.log(event.target.name,event.target.value)
+  if (event.target.name === 'email') {
+    const isEmailValid = /\S+@\S+\.\S+/.test(event.target.value);
+    console.log(isEmailValid)
+  }
+  if (event.target.name === 'password') {
+    const isPassWordValid = event.target.value.length > 5;
+    const passwordHasNumber = /\d{3}/.test (event.target.value);
+    console.log(passwordHasNumber && isPassWordValid)
+  }
+}
+
+  return (
+    <div style={{marginLeft:'45%',marginTop:'10%'}}>
+       {
+        user.isSignIn ? 
+          <button onClick={handleSignOut} style={{color:'red'}}>sign out</button>
+          :<button onClick={handleSingIn} style={{color:'red'}}>sign in</button>
       }
 
       {
-        user.isSignIn  && <div><p>Welcome,{user.name}</p>
+        user.isSignIn  && <div>
+        <p>Welcome,{user.name}</p>
         <p>Email:{user.email}</p>
         <img src={user.photo} alt=""/>
+
       </div>
-      }
-    </div>
+
+
+      } 
+       <form onSubmit={handleSubmit}>
+         <input type="text" name="email" onBlur={handleBlur} placeholder="Enter your Email" required/>
+         <br/>
+         <input type="password" name="password" onBlur={handleBlur}  id="" placeholder="Enter your password" required/>
+         <br/>
+         <input type="submit" value="Submit"/>
+         </form>       
+
+      
+     </div>
   );
 }
 
 export default App;
+ 
+
+// Module No: 42.. video No:1/2/3/
+// const handleBlur = (event) => {
+//   console.log(event.target.name,event.target.value)
+//   if (event.target.name === 'email') {
+//     const isEmailValid = /\S+@\S+\.\S+/.test(event.target.value);
+//     console.log(isEmailValid)
+//   }
+//   if (event.target.name === 'password') {
+//     const isPassWordValid = event.target.value.length > 5;
+//     const passwordHasNumber = /\d{3}/.test (event.target.value);
+//     console.log(passwordHasNumber && isPassWordValid)
+//   }
+// }
+
+//   return (
+//     <div style={{marginLeft:'45%',marginTop:'10%'}}>
+//        {
+//         user.isSignIn ? 
+//           <button onClick={handleSignOut} style={{color:'red'}}>sign out</button>
+//           :<button onClick={handleSingIn} style={{color:'red'}}>sign in</button>
+//       }
+
+//       {
+//         user.isSignIn  && <div>
+//         <p>Welcome,{user.name}</p>
+//         <p>Email:{user.email}</p>
+//         <img src={user.photo} alt=""/>
+
+//       </div>
+
+
+//       } 
+//        <form onSubmit={handleSubmit}>
+//          <input type="text" name="email" onBlur={handleBlur} placeholder="Enter your Email" required/>
+//          <br/>
+//          <input type="password" name="password" onBlur={handleBlur}  id="" placeholder="Enter your password" required/>
+//          <br/>
+//          <input type="submit" value="Submit"/>
+//          </form>       
+
+      
+//      </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import *as firebase from "firebase/app";
+// import 'firebase/auth';
+// import firebaseConfig from '../'
+// firebase.initializeApp(firebaseConfig);
+// const Home = () => {
+//     const provider = new firebase.auth.GoogleAuthProvider()
+//     const handleSignIn = () => {
+//         firebase.auth().signInWithPopup(provider)
+//         .then(res => {
+//            console.log(res.user)
+//         })
+//     }
+//     return (
+//         <div>
+//             <h2>raju</h2>
+//             <button onClick ={handleSignIn}>Sign In</button>
+        
+//         </div>
+//     );
+// };
+
+// export default Home;
